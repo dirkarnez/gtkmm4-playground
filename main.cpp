089875d25daa5d964dc73bc0c96a41322ed11c16
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-14 20:19:19 +0800

    - update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
-#include <gtkmm.h>
+#include <iostream>
+#include <gtkmm/application.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
 
-int main(int argc, char** argv)
+class HelloWorld : public Gtk::Window
 {
-        auto app = Gtk::Application::create(argc, argv);
-        
-        Gtk::Window window;
-        window.set_default_size(600,400);
-        
-        return app->run(window);
+
+public:
+  HelloWorld();
+  ~HelloWorld() override;
+
+protected:
+  //Signal handlers:
+  void on_button_clicked();
+
+  //Member widgets:
+  Gtk::Button m_button;
+};
+
+HelloWorld::HelloWorld()
+: m_button("Hello World")   // creates a new button with label "Hello World".
+{
+  // Sets the margin around the button.
+  m_button.set_margin(10);
+
+  // When the button receives the "clicked" signal, it will call the
+  // on_button_clicked() method defined below.
+  m_button.signal_clicked().connect(sigc::mem_fun(*this,
+              &HelloWorld::on_button_clicked));
+
+  // This packs the button into the Window (a container).
+  set_child(m_button);
+}
+
+HelloWorld::~HelloWorld()
+{
+}
+
+void HelloWorld::on_button_clicked()
+{
+  std::cout << "Hello World" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+  auto app = Gtk::Application::create("org.gtkmm.example");
+
+  //Shows the window and returns when it is closed.
+  return app->make_window_and_run<HelloWorld>(argc, argv);
 }
